a3c3fe07b2f2742d98818a60e95433a94b9e21b4
pwrstudio <pwrpntr@gmail.com>
2021-08-30 12:06:35 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  // # # # # # # # # # # # # #
+  //
+  //  TSoaP Client
+  //
+  // # # # # # # # # # # # # #
+
+  // IMPORTS
+  import { Router, Route } from "svelte-routing"
+
+  // ROUTES
+  import World from "./World.svelte"
+</script>
+
+<Router>
+  <Route path="/*" let:params>
+    <World {params} />
+  </Route>
+</Router>
