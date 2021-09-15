6098722ff9fb6685a04bd1cbc6195148878d958d
pwrstudio <pwrpntr@gmail.com>
2021-09-15 17:23:44 +0200

    Work on menubar

--- src/Clock.svelte
+++ src/Clock.svelte
@@ -5,26 +5,44 @@
   //
   // # # # # # # # # # # # # #
 
-  function berlinTime() {
-    var d = new Date()
-    var cet = new Intl.DateTimeFormat("nyc-US", {
-      timeZone: "EST",
+  let berlinTime
+  let newYorkTime
+  let seoulTime
+
+  const updateTime = () => {
+    let d = new Date()
+    newYorkTime = new Intl.DateTimeFormat("en-US", {
+      timeZone: "America/New_York",
       hour: "numeric",
       minute: "numeric",
     }).format(d)
-    time = cet
-    return cet
+
+    berlinTime = new Intl.DateTimeFormat("nyc-US", {
+      timeZone: "Europe/Berlin",
+      hour: "numeric",
+      minute: "numeric",
+    }).format(d)
+
+    seoulTime = new Intl.DateTimeFormat("nyc-US", {
+      timeZone: "Asia/Seoul",
+      hour: "numeric",
+      minute: "numeric",
+    }).format(d)
   }
 
-  var time = berlinTime()
-  window.setInterval(berlinTime, 10000)
-  berlinTime()
+  window.setInterval(updateTime, 10000)
+  updateTime()
 </script>
 
 <div class="clock-berlin">
-  <span class="time">{time}</span>New York City
+  <select>
+    <option>{newYorkTime} in New York City</option>
+    <option>{berlinTime} in Berlin</option>
+    <option>{seoulTime} in Seoul</option>
+  </select>
 </div>
 
+<!-- <span class="time">{time}</span> -->
 <style lang="scss">
   @import "./variables.scss";
 
@@ -41,5 +59,23 @@
       color: $COLOR_LIGHT;
       text-decoration: none;
     }
+  }
+
+  select {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    display: block;
+    width: 100%;
+    margin: 0;
+    padding: 10px;
+    font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+    color: inherit;
+    background-color: transparent;
+    border: 0;
+    border-radius: 0;
+    outline: 0;
+    cursor: pointer;
   }
 </style>
--- src/Menubar.svelte
+++ src/Menubar.svelte
@@ -11,10 +11,23 @@
 
   // *** STORES
   import { currentArea } from "./stores.js"
+
+  let selectValue = ""
+
+  $: if (selectValue === "https://eyebeam.org") {
+    window.location.href = "https://eyebeam.org"
+  }
 </script>
 
 <div class="menubar" use:links>
-  <div class="title">open.eyebeam.org : {$currentArea}</div>
+  <!-- TITLE -->
+  <div class="title">
+    <select bind:value={selectValue}>
+      <option value="https://open-eyebeam.netlify.app">open.eyebeam.org</option>
+      <option value="https://eyebeam.org">eyebeam.org</option>
+    </select>
+  </div>
+  <!-- CLOCK -->
   <Clock />
 </div>
 
@@ -33,9 +46,28 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding-left: 30px;
-    padding-right: 30px;
+    padding-left: 20px;
+    padding-right: 20px;
     color: $COLOR_LIGHT;
     font-size: $FONT_SIZE_SMALL;
+  }
+
+  select {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    display: block;
+    width: 100%;
+    margin: 0;
+    padding: 10px;
+    font-family: inherit;
+    font-size: inherit;
+    line-height: 1.5em;
+    color: inherit;
+    background-color: transparent;
+    border: 0;
+    border-radius: 0;
+    outline: 0;
+    cursor: pointer;
+    overflow: visible;
   }
 </style>
