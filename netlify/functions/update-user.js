74e71c51e5919cfa87b2dbfc334faab2d65657ea
pwrstudio <pwrpntr@gmail.com>
2022-03-16 15:10:24 +0100

    Work on user update function

--- netlify/functions/update-user.js
+++ netlify/functions/update-user.js
@@ -2,24 +2,24 @@ const sanity = require("@sanity/client")
 const _ = require('lodash')
 const { v4: uuidv4 } = require('uuid');
 
-const client = sanity({
+const sanityClient = sanity({
     projectId: process.env.SANITY_ID,
     dataset: "production",
     token: process.env.SANITY_TOKEN,
     useCdn: false,
 })
 
-const loadData = async (query, params) => {
-    try {
-        const res = await client.fetch(query, params)
-        if (res === null) {
-            return Promise.reject('No posts');
-        }
-        return res
-    } catch (err) {
-        return Promise.reject(new Error(404));
-    }
-}
+// const loadData = async (query, params) => {
+//     try {
+//         const res = await client.fetch(query, params)
+//         if (res === null) {
+//             return Promise.reject('No posts');
+//         }
+//         return res
+//     } catch (err) {
+//         return Promise.reject(new Error(404));
+//     }
+// }
 
 const HEADERS = {
     'Access-Control-Allow-Origin': '*',
@@ -29,16 +29,27 @@ const HEADERS = {
 }
 
 exports.handler = async (event, context) => {
-    if (true) {
-        return {
-            statusCode: 200,
-            headers: HEADERS,
-            body: JSON.stringify(event)
-        };
+    console.log(event)
+    const authObject = JSON.parse(event.body)
+    console.log('authObject', authObject)
+    const currentDoc = await sanityClient.fetch("*[_type == 'user' && _id == $userId][0]", { userId: authObject.id })
+    console.log('currentDoc', currentDoc)
+    const doc = {
+        _type: 'user',
+        _id: authObject.id,
+        name: authObject.username,
+        // avatarURL: event.user.picture,
+        // roles: roles,
+        email: authObject.email,
+        slug: {
+            _type: 'slug',
+            current: slug(authObject.username)
+        }
     }
+    const newDoc = await sanityClient.createOrReplace(doc)
     return {
-        statusCode: 500,
+        statusCode: 200,
         headers: HEADERS,
-        body: "ERROR"
+        body: JSON.stringify(newDoc)
     };
 }
